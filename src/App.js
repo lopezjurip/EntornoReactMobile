abeecf54ea136c44293dd5deb36ef6320f0a4233
Patricio López <lopezjuripatricio@gmail.com>
2016-08-17 01:02:38 -0300

    Make use of common App component

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello world</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
+
+export default App;
